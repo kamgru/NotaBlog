ce5a33e5cd63c0220ea81661b0054c2691597b0b
kamgru <kamil.grudzien@hotmail.com>
2017-11-26 14:45:21 +0100

    add stories controller

---
+++ NotaBlog.Website/Controllers/StoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NotaBlog.Api;
+
+namespace NotaBlog.Website.Controllers
+{
+    public class StoriesController : Controller
+    {
+        private readonly StoryService _storyService;
+
+        public StoriesController(StoryService storyService)
+        {
+            _storyService = storyService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var stories = await _storyService.GetLatestStories(5);
+            return View(stories);
+        }
+    }
+}
