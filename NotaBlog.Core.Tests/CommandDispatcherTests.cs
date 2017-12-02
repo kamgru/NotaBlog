c68076671503c23818fc8fb706cba20f132f64dc
kamgru <kamil.grudzien@hotmail.com>
2017-12-02 10:30:23 +0100

    add command dispatcher
    who are we? engineers! what do we want? more abstraction! where do we want it? everywhere!

---
+++ NotaBlog.Core.Tests/CommandDispatcherTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using NotaBlog.Core.Commands;
+using NotaBlog.Tests.Common.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NotaBlog.Core.Tests
+{
+    public class CommandDispatcherTests
+    {
+        [Fact]
+        public void WhenHandlerNotFound_ItShouldFail()
+        {
+            var dispatcher = new CommandDispatcher();
+
+            var result = dispatcher.Submit(new MockCommand()).Result;
+
+            result.Success.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ItShouldHandleCommand()
+        {
+            var dispatcher = new CommandDispatcher();
+            var handler = new MockCommandHandler();
+
+            dispatcher.RegisterHandler(handler);
+
+            var result = dispatcher.Submit(new MockCommand()).Result;
+
+            handler.WasHandled.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ItShouldReturnHandlerCommandValidationResult()
+        {
+            var commandValidationResult = new CommandValidationResult
+            {
+                Errors = new string[0]
+            };
+
+            var handler = new MockCommandHandler
+            {
+                CommandValidationResult = commandValidationResult
+            };
+
+            var dispatcher = new CommandDispatcher();
+            dispatcher.RegisterHandler(handler);
+
+            var result = dispatcher.Submit(new MockCommand()).Result;
+
+            result.Should().BeSameAs(commandValidationResult);
+        }
+    }
+}
