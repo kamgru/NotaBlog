de96e8678a6db2e746a6bd9b8cd6071226d2b665
kamgru <kamil.grudzien@hotmail.com>
2017-12-23 13:58:37 +0100

    update story header model

--- NotaBlog.Admin/ClientApp/app/stories/models/IStoryHeader.ts
+++ NotaBlog.Admin/ClientApp/app/stories/models/IStoryHeader.ts
@@ -1,4 +1,7 @@
 export interface IStoryHeader {
     id:string,
     title:string,
+    publicationStatus:number,
+    created:Date,
+    publihed:Date | null
 }
--- NotaBlog.Admin/ClientApp/app/stories/stories.service.ts
+++ NotaBlog.Admin/ClientApp/app/stories/stories.service.ts
@@ -4,19 +4,11 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { IStory } from './models/IStory';
 
-const data:IStoryHeader[] = [
-    {id:"1", title: "fake1"},
-    {id:"2", title: "fake2"},
-    {id:"3", title: "fake3"},
-    {id:"4", title: "fake4"},
-    {id:"5", title: "fake5"},
-];
-
 @Injectable()
 export class StoriesService {
 
     public getStoryHeaders(): Observable<IStoryHeader[]> {
-        return of(data);
+        return of([]);
     }
 
     public getStory(id:string): Observable<IStory> {
